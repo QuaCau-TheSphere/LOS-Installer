94f039e941a17735a22256690aa0e136cf3b40f1
Ooker <ganuongphap@gmail.com>
2022-12-16 23:58:15 +0700

    cơ bản là xong

--- Resources/Script cài font.bat
+++ Resources/Script cài font.bat
@@ -1,4 +1,4 @@
 copy "SVN-Avo.ttf" "%WINDIR%\Fonts"
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "SVN-Avo (TrueType)" /t REG_SZ /d SVN-Avo.ttf /f
 
-./LOS Installer.exe
+"..\LOS Installer.exe"
