ef4863b708d898756c4f97ebcaf97a68b17115cd
ooker <ganuongphap@gmail.com>
2022-12-20 00:37:35 +0700

    tự động trả lời yes khi cài .Net

--- Setup/Pre-open.bat
+++ Setup/Pre-open.bat
@@ -4,5 +4,5 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "SVN-Avo (T
 REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\AppHost" /v EnableWebContentEvaluation /t REG_DWORD /d 0
 REG ADD "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\ Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\PhishingFilter" /v EnabledV9 /t REG_DWORD /d 0
  
-winget install Microsoft.DotNet.DesktopRuntime.7 --accept-source-agreements --accept-package-agreements 
+ECHO Y | winget install Microsoft.DotNet.DesktopRuntime.7 --accept-source-agreements --accept-package-agreements 
 "temp.exe"
